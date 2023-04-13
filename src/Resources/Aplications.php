1a8f362f4da29f0d740b8ee96e5d2bc8bcee6376
Guilherme Almeida Girardi <guilhermeagirardi@gmail.com>
2023-04-13 00:19:54 -0300

    0.1.4

--- src/Resources/Aplications.php
+++ src/Resources/Aplications.php
@@ -3,30 +3,30 @@
 namespace Ejetar\AzureADAPIClient\Resources;
 
 class Aplications extends Base {
-    protected string $resource = 'applications';
+	protected string $resource = 'applications';
 
-    public function create(array $data, array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->post($this->ref(), $data, $accessToken, $headers);
-    }
+	public function create(array $data, array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return $this->provider->post($this->ref(), $data, $accessToken, $headers);
+	}
 
-    public function list(array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return collect($this->provider->get($this->ref(),$accessToken,$headers));
-    }
+	public function list($params = "", array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return collect($this->provider->get($this->ref() . "?$params", $accessToken, $headers));
+	}
 
-    public function get($userPrincipalName, array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->get($this->ref("/$userPrincipalName"),$accessToken,$headers);
-    }
+	public function get($userPrincipalName, array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return $this->provider->get($this->ref("/$userPrincipalName"), $accessToken, $headers);
+	}
 
-    public function update($userPrincipalName, array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->delete($this->ref("/$userPrincipalName"),$accessToken,$headers);
-    }
+	public function update($userPrincipalName, array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return $this->provider->delete($this->ref("/$userPrincipalName"), $accessToken, $headers);
+	}
 
-    public function delete(array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->delete($this->ref(),$accessToken,$headers);
-    }
+	public function delete(array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return $this->provider->delete($this->ref(), $accessToken, $headers);
+	}
 }
--- src/Resources/ServicePrincipals.php
+++ src/Resources/ServicePrincipals.php
@@ -3,45 +3,45 @@
 namespace Ejetar\AzureADAPIClient\Resources;
 
 class ServicePrincipals extends Base {
-    protected string $resource = 'servicePrincipals';
+	protected string $resource = 'servicePrincipals';
 
-    public function create(array $data, array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->post($this->ref(), $data, $accessToken, $headers);
-    }
-
-    public function list(array $headers = [],$doNotWrap = false) {
-        $accessToken = request()->session()->get('access_token');
-        return collect($this->provider->get($this->ref(),$accessToken,$headers,$doNotWrap));
-    }
-
-    public function get($userPrincipalName, array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->get($this->ref("/$userPrincipalName"),$accessToken,$headers);
-    }
-
-    public function update($userPrincipalName, array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->delete($this->ref("/$userPrincipalName"),$accessToken,$headers);
-    }
-
-    public function delete(array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return $this->provider->delete($this->ref(),$accessToken,$headers);
-    }
-
-    public function assignments($userPrincipalName, array $headers = []) {
-        $accessToken = request()->session()->get('access_token');
-        return collect($this->provider->get($this->ref("/$userPrincipalName/appRoleAssignments"),$accessToken,$headers));
-    }
-
-	public function appRoleAssignedTo($userPrincipalName,$params = "",array $headers = []) {
+	public function create(array $data, array $headers = []) {
 		$accessToken = request()->session()->get('access_token');
-		return collect($this->provider->get($this->ref("/$userPrincipalName/appRoleAssignedTo")."?$params",$accessToken,$headers));
+		return $this->provider->post($this->ref(), $data, $accessToken, $headers);
 	}
 
-	public function getObjects($tenant, array $headers = []) {
+	public function list(array $headers = [], $params = "", $doNotWrap = false) {
 		$accessToken = request()->session()->get('access_token');
-		return collect($this->provider->getObjects($tenant, $this->ref(),$accessToken,$headers));
+		return collect($this->provider->get($this->ref() . "?$params", $accessToken, $headers, $doNotWrap));
+	}
+
+	public function get($userPrincipalName, $params = "", array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return $this->provider->get($this->ref("/$userPrincipalName") . "?$params", $accessToken, $headers);
+	}
+
+	public function update($userPrincipalName, array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return $this->provider->delete($this->ref("/$userPrincipalName"), $accessToken, $headers);
+	}
+
+	public function delete(array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return $this->provider->delete($this->ref(), $accessToken, $headers);
+	}
+
+	public function assignments($userPrincipalName, $params = "", array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return collect($this->provider->get($this->ref("/$userPrincipalName/appRoleAssignments") . "?$params", $accessToken, $headers));
+	}
+
+	public function appRoleAssignedTo($userPrincipalName, $params = "", array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return collect($this->provider->get($this->ref("/$userPrincipalName/appRoleAssignedTo") . "?$params", $accessToken, $headers));
+	}
+
+	public function getObjects($tenant, $params = "", array $headers = []) {
+		$accessToken = request()->session()->get('access_token');
+		return collect($this->provider->getObjects($tenant, $this->ref() . "?$params", $accessToken, $headers));
 	}
 }
